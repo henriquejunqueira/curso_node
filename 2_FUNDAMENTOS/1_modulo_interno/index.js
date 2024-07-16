b97b49246ef5b321883ff30ac463c17025cd4943
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-07-16 18:48:30 -0300

    módulos internos

---
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -0,0 +1,9 @@
+// * importa o módulo. É necessário o ./ para dizer que é um módulo interno. Não é necessário a extensão
+const meuModulo = require('./meu_modulo');
+
+// * encapsula o módulo em uma variável sem os parênteses, pois com parênteses estará executando a função
+const soma = meuModulo.soma;
+
+// * utiliza a função soma do módulo
+soma(2, 3); // ? 5
+soma(5, 10); // ? 15
---
+++ 2_FUNDAMENTOS/1_modulo_interno/meu_modulo.js
@@ -0,0 +1,6 @@
+module.exports = {
+	// ? quando é exporta uma função no módulo, não é necessário colocar a palavra function
+	soma(a, b) {
+		console.log(a + b);
+	},
+};
